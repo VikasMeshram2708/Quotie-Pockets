73f62f6b2ddc871a9e95cb1062d6c1f3e76ee3a0
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-27 13:01:16 +0530

    :art: Created Privacy Policy page.

--- src/page/Privacy.tsx
+++ src/page/Privacy.tsx
@@ -1,5 +1,79 @@
 export default function Privacy() {
   return (
-    <div>Privacy</div>
-  )
+    <section className="prose lg:prose-xl text-justify mt-14 mx-auto">
+      <h1 className="text-center">Privacy Policy</h1>
+      <p>
+        Quotie Pockets is committed to protecting the privacy of its users. This
+        Privacy Policy outlines the types of personal information that is
+        received and collected by Quotie Pockets and how it is used.
+      </p>
+      <div>
+        <h4>Information Collection and Use</h4>
+        <ul>
+          <li>
+            Log Data: Like many website operators, we collect information that
+            your browser sends whenever you visit our site. This Log Data may
+            include information such as your computer's Internet Protocol (IP)
+            address, browser type, browser version, the pages of our site that
+            you visit, the time and date of your visit, the time spent on those
+            pages, and other statistics.
+          </li>
+          <li>
+            Cookies: We use cookies to store information about visitors'
+            preferences, to record user-specific information on which pages the
+            user accesses or visits, and to personalize or customize our web
+            page content based upon visitors' browser type or other information
+            that the visitor sends via their browser.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Information Sharing and Disclosure</h4>
+        <ul>
+          <li>
+            Third-Party Services: We may employ third-party companies and
+            individuals to facilitate our service, to provide the service on our
+            behalf, to perform service-related services, or to assist us in
+            analyzing how our service is used. These third parties have access
+            to your personal information only to perform these tasks on our
+            behalf and are obligated not to disclose or use it for any other
+            purpose.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Security</h4>
+        <ul>
+          <li>
+            We strive to use commercially acceptable means to protect your
+            personal information. However, no method of transmission over the
+            Internet, or method of electronic storage, is 100% secure and
+            reliable, and we cannot guarantee its absolute security.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Changes to This Privacy Policy</h4>
+        <ul>
+          <li>
+            We may update our Privacy Policy from time to time. We will notify
+            you of any changes by posting the new Privacy Policy on this page.
+            You are advised to review this Privacy Policy periodically for any
+            changes.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Contact Us</h4>
+        <p>
+          If you have any questions about this Privacy Policy, please contact
+          us.
+        </p>
+      </div>
+    </section>
+  );
 }

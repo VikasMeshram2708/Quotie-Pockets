7fa380b90b8471a3c3f1260d5d85cf4f0e792c65
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-02-27 12:32:35 +0530

    :zap: Created form validation using react hook form.

--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -1,62 +1,84 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type InputType = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<InputType>();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      const data = {
-        name,
-        email,
-        message,
-      };
-      console.log("data", data);
-    } catch (error) {
-      console.log(
-        error instanceof Error
-          ? error?.message
-          : "Something went wrong try again."
-      );
-    }
+  const onSubmit: SubmitHandler<InputType> = (data) => {
+    console.log("data", data);
+    reset();
   };
+
   return (
     <section className="max-w-2xl mx-auto mt-24">
       <h1 className="text-5xl text-center mb-10">Contact Us</h1>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-5">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-3 p-5"
+      >
         <div className="grid grid-cols-2 gap-3">
-          <input
-            className="outline-none p-3 rounded"
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              setName(event.target.value)
-            }
-          />
-          <input
-            className="outline-none p-3 rounded"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(event: ChangeEvent<HTMLInputElement>) =>
-              setEmail(event.target.value)
-            }
-          />
+          <div>
+            <input
+              className="outline-none w-full p-3 rounded"
+              type="text"
+              placeholder="Name"
+              {...register("name", {
+                required: "Name is required",
+                minLength: {
+                  value: 2,
+                  message: "Name must be at least 2 characters",
+                },
+              })}
+            />
+            {errors.name && (
+              <p className="text-red-500 font-semibold">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <input
+              className="outline-none p-3 rounded w-full"
+              type="email"
+              placeholder="Email"
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && (
+              <p className="text-red-500 font-semibold">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
         </div>
-        <div className="">
+        <div>
           <textarea
             rows={5}
             className="w-full p-2 outline-none rounded"
             placeholder="Message"
-            value={message}
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-              setMessage(event.target.value)
-            }
+            {...register("message", {
+              required: "Message is required",
+              minLength: {
+                value: 2,
+                message: "Message must be at least 2 characters",
+              },
+            })}
           ></textarea>
+          {errors.message && (
+            <p className="text-red-500 font-semibold">
+              {errors.message.message}
+            </p>
+          )}
         </div>
         <button
           type="submit"

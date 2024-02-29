076d0f24273cc6c55fdd793ff3fa9e826e55fc89
kaali <meshramvikas744@gmail.com>
2024-02-29 17:42:59 +0530

    :fire: tested the get users email api.

--- test/contact.test.ts
+++ test/contact.test.ts
@@ -2,15 +2,10 @@ import { describe, it, expect } from "vitest";
 
 describe("Contact Route", () => {
   it("hit the api", async () => {
-    const response = await fetch("http://localhost:5000/api/contact-us");
+    const response = await fetch("http://localhost:5000/api/get-contact-registered-email");
     const result = await response.json();
-    console.log("res", result);
-    // if (!response.ok) {
-    //   const error = await response.text();
-    //   expect(error).toMatch(/Error: Not Found/);
-    //   return;
-    // }
-    // const result = await response.json();
-    // expect(result).toBeDefined();
+    // const totalResults = result.result.length;
+    expect(result).toBeDefined();
+    // expect(result.result).toHaveLength(totalResults);
   });
 });

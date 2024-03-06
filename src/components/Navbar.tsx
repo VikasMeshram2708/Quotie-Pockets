53434f41061e62749c27543a8e65d7a6dc1473a2
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-06 17:22:06 +0530

    :bug: fixed the cached bug.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,8 +41,7 @@ export default function Navbar() {
   };
   const [toggleModal, setToggleModal] = useState(false);
 
-  useEffect(() => {
-  }, [isAuthenticated]);
+  useEffect(() => {}, [isAuthenticated]);
 
   return (
     <nav className="border-b-4 border-[--pprl] text-white h-24 p-2 flex flex-wrap gap-5 items-center justify-between max-w-[90%] mx-auto">
@@ -69,8 +68,10 @@ export default function Navbar() {
       <div>
         {isAuthenticated ? (
           <button
-            onClick={() => {
+            onClick={async () => {
               destroyCookie(null, "quotieAuth");
+              alert("Logged Out Successfully.");
+              return window.location.reload();
             }}
             type="button"
             className="px-4 py-2 rounded-lg border-4 border-[--pprl]  font-semibold text-[1.2rem]"

d3bfd91c3a177ce923094d18a06f4381c4e19b68
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-08 00:13:15 +0530

    :sparkles: Added form layout.

--- app/quoties/page.tsx
+++ app/quoties/page.tsx
@@ -2,6 +2,12 @@ export default function Quoties() {
   return (
     <section>
       <h1>Quoties Page</h1>
+
+      <form className="max-w-2xl mx-auto p-5 grid gap-5">
+        <input type="text" className="p-2 rounded" placeholder="Enter title" />
+        <textarea className="p-2 rounded" placeholder="Enter message" rows={5}></textarea>
+        <button type="button" className="btn btn-primary-200 border">Submit</button>
+      </form>
     </section>
   );
 }

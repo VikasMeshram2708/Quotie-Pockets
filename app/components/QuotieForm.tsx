46e820387a79fa9d3b951d1021716ce65403b6b7
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-09 16:41:24 +0530

    :white_check_mark: mocking the quoties form.

--- app/components/QuotieForm.tsx
+++ app/components/QuotieForm.tsx
@@ -1,34 +1,102 @@
 "use client";
 
-import { SubmitQuotie } from "@/lib/SubmitQuotie";
-import React, { useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { QuotiesSchema } from "../api/models/QuotiesModel";
+import slugify from "slugify";
+
 
 export default function QuotieForm() {
-  const ref = useRef<HTMLFormElement>(null);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<QuotieType>();
+
+  const handleQuoties: SubmitHandler<QuotieType> = (data) => {
+    try {
+      const { title, message } = data;
+      const slug = slugify(title);
+
+      const quotiesData = {
+        title,
+        slug,
+        message,
+        author: "65eadc861cdc44d32f3137e7",
+      };
+
+      QuotiesSchema.parse(quotiesData);
+      console.log("quoties", quotiesData);
+      return reset();
+    } catch (e) {
+      const err = e as Error;
+      if (e instanceof z.ZodError) {
+        return alert(e.errors[0].message);
+      }
+      return alert(err.message);
+    }
+  };
+
   return (
     <>
       <form
-        ref={ref}
-        action={async (formData) => {
-          await SubmitQuotie(formData);
-          ref.current?.reset();
-        }}
+        onSubmit={handleSubmit(handleQuoties)}
         className="mt-10 max-w-2xl mx-auto p-5 grid gap-5"
       >
-        <input
-          name="title"
-          id="title"
-          type="text"
-          className="p-2 rounded"
-          placeholder="Enter title"
-        />
-        <textarea
-          name="message"
-          id="title"
-          className="p-2 rounded"
-          placeholder="Enter message"
-          rows={5}
-        ></textarea>
+        <div className="grid gap-3">
+          <input
+            type="text"
+            className="p-2 rounded"
+            placeholder="Enter title"
+            {...register("title", {
+              min: {
+                value: 2,
+                message: "Title must be at least 2 characters long",
+              },
+              max: {
+                value: 150,
+                message: "Title must be at least 150 characters long",
+              },
+              required: {
+                value: true,
+                message: "Title is required",
+              },
+            })}
+          />
+          {errors?.title && (
+            <p className="text-red-500 font-semibold">
+              {errors?.title?.message}
+            </p>
+          )}
+        </div>
+        <div className="grid gap-3">
+          <textarea
+            className="p-2 rounded"
+            placeholder="Enter message"
+            rows={5}
+            {...register("message", {
+              min: {
+                value: 2,
+                message: "Title must be at least 2 characters long",
+              },
+              max: {
+                value: 150,
+                message: "Title must be at least 150 characters long",
+              },
+              required: {
+                value: true,
+                message: "Title is required",
+              },
+            })}
+          ></textarea>
+          {errors?.message && (
+            <p className="text-red-500 font-semibold">
+              {errors?.message?.message}
+            </p>
+          )}
+        </div>
+
         <button type="submit" className="btn btn-primary-200 border">
           Submit
         </button>
